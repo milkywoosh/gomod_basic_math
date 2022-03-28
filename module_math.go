ca141fca2d9c9464b7582f8e2e229cbbbd0beba2
lukmanx <archer.lukman@gmail.com>
2022-03-28 22:25:12 +0700

    test

--- module_math.go
+++ module_math.go
@@ -1,12 +1,12 @@
 package gomod_basic_math
 
-func Math_add(data ...int) (result int) {
-	result = 0
-	for _, value := range data {
-		result += value
-	}
-	return result
-}
+// func Math_add(data ...int) (result int) {
+// 	result = 0
+// 	for _, value := range data {
+// 		result += value
+// 	}
+// 	return result
+// }
 
 func Math_power(value float64, pow int) int {
 	for i := pow; i > 1; i-- {
