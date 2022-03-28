a517148d1197cb4accbe2f29a09481d4fbc8a4fe
lukmanx <archer.lukman@gmail.com>
2022-03-28 22:28:03 +0700

    add and power

--- module_math.go
+++ module_math.go
@@ -1,16 +1,17 @@
 package gomod_basic_math
 
-// func Math_add(data ...int) (result int) {
-// 	result = 0
-// 	for _, value := range data {
-// 		result += value
-// 	}
-// 	return result
-// }
+func Math_add(data ...int) (result int) {
+	result = 0
+	for _, value := range data {
+		result += value
+	}
+	return result
+}
 
 func Math_power(value float64, pow int) int {
+	tmp := value
 	for i := pow; i > 1; i-- {
-		value *= value
+		value *= tmp
 	}
 	return int(value)
 }
